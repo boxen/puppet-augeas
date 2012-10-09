657a3994bb4f2c973db72032fd0d25645030086c
Tim Sharpe <tim@github.com>
2012-10-10 08:01:00 +1100

    Update specs for dd38a33d /cc @wfarr

--- spec/classes/augeas_spec.rb
+++ spec/classes/augeas_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'augeas' do
   it do
-    should contain_package('augeas').with({
+    should contain_package('boxen/brews/augeas').with({
       :ensure => '0.8.1-boxen2'
     })
   end
